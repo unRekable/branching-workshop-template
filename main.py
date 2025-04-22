dadf28bdca68c5961570706deb53914797bd0bf8
juan-masterschool <167711474+juan-masterschool@users.noreply.github.com>
2025-04-22 22:54:36 +0200

    Initial main.py
    main.py ready to be expanded!

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("Length: 5000 meters = ", meters_to_kilometers(5000), "kilometers")
+    print("Weight: 10000 grams = ", grams_to_kilograms(10000), "kilograms")
+    print("Temperature: 37°C = ", celsius_to_fahrenheit(37), "°F")
+    print("Volume: 3 liters = ", liters_to_milliliters(3), "milliliters")
+    print("Area: 1000000 square meters = ", square_meters_to_square_kilometers(1000000), "square kilometers")
+    print("Speed: 10 m/s = ", mps_to_kph(10), "km/h")
+    print("Time: 120 seconds = ", seconds_to_minutes(120), "minutes")
+    print("Data: 2048 bytes = ", bytes_to_kilobytes(2048), "kilobytes")
+
+if __name__ == "__main__":
+    main()
