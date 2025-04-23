e7066fe2eee52f0e6c3f31d398ce43658c263888
juan-masterschool <167711474+juan-masterschool@users.noreply.github.com>
2025-04-23 08:54:24 +0200

    Update main.py
    Updated main so there is no need at all to modify it, not even the imports!

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from length_converter import meters_to_kilometers
+from weight_converter import grams_to_kilograms
+from temperature_converter import celsius_to_fahrenheit
+from volume_converter import liters_to_milliliters
+from area_converter import square_meters_to_square_kilometers
+from speed_converter import mps_to_kph
+from time_converter import seconds_to_minutes
+from data_converter import bytes_to_kilobytes
+
 def main():
     print("Length: 5000 meters = ", meters_to_kilometers(5000), "kilometers")
     print("Weight: 10000 grams = ", grams_to_kilograms(10000), "kilograms")
