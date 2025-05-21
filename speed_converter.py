570b69453339187cc6142160510ca88b2f326787
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-21 13:14:24 +0200

    Add speed convertion

---
+++ speed_converter.py
@@ -0,0 +1,7 @@
+def mps_to_kph(speed_in_mps):
+    """
+    Converts speed from mps to kph
+    :param speed_in_mps: speed in meter per second
+    :return: speed in kilometer per hour
+    """
+    return speed_in_mps * 3.6
