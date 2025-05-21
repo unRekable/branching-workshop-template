bd425998d5c5aa76413d1c092af66be419660e9b
AndErem314 <andrey@Mac.lan>
2025-05-21 13:05:20 +0200

    first commit

--- time_converter.py
+++ time_converter.py
@@ -1,3 +1,6 @@
 
 def seconds_to_minutes(seconds):
-    return seconds / 60
+    """Convert seconds to minutes and remaining seconds."""
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+    return minutes, remaining_seconds
